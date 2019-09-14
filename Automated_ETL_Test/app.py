5117d07bd6d320f078fa642177b2cf48d6e4c53e
vabigdatamover <vabigdatamover@gmail.com>
2019-09-14 10:08:07 -0400

    ETL Project

--- Automated_ETL_Test/app.py
+++ Automated_ETL_Test/app.py
@@ -24,7 +24,19 @@ def home():
 
 
 # Route that will trigger the scrape function
-@app.route("/scrape")
+@app.route("/schedule")
+def scrape():
+    
+    # Run the scrape function
+    mars_data=scrape_mars.scrape_info()
+    
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+    
+    # Redirect back to home page
+    return redirect("/")
+
+@app.route("/stop")
 def scrape():
     
     # Run the scrape function
